85b15e3c0be8364bc1231f2e93d7b7901bcc90eb
Peter Jang <peter.x.jang@gmail.com>
2018-07-31 15:09:27 -0500

    Add post model with fake data and RESTful API actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+
+    get "/posts" => "posts#index"
+    post "/posts" => "posts#create"
+    get "/posts/:id" => "posts#show"
+    patch "/posts/:id" => "posts#update"
+    delete "/posts/:id" => "posts#destroy"
   end
 end
